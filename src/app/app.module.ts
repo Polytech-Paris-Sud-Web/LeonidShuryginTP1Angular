bc1647fd90b5139be3a697e71f935928bbe0af81
Léonid Shurygin <leonid.shurygin@u-psud.fr>
2022-05-18 13:42:29 +0200

    add sidebar and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleService } from './article.service';
 import { ArticleCreationComponent } from './article-creation/article-creation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Angular Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
+const appRoutes: Routes = [
+  { path: 'create', component: ArticleCreationComponent },
+  { path: 'articleList', component: ArticleListComponent },
+  { path: '', component: ArticleListComponent }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,10 +32,29 @@ import { ArticleCreationComponent } from './article-creation/article-creation.co
     ArticleCreationComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule,
-    ReactiveFormsModule
+    //AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    //Angular Material
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatDividerModule, 
+  ],
+  exports: [
+    //Angular Material
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatDividerModule, 
   ],
   providers: [ArticleService],
   bootstrap: [AppComponent]
